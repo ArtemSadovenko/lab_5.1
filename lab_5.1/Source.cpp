c2f976fab9a5dc1f33836e57994659c53622e901
artem sadovenko <asadovenko7@gmail.com>
2021-10-20 23:32:10 +0300

    5.1_com_1

---
+++ lab_5.1/Source.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double g(double x, double y);
+
+int main(){
+	double s;
+	double r;
+	double k;
+
+	cout << "s = "; cin >> s;
+	cout << "r = "; cin >> r;
+
+	k = (pow(g(r, s * s + 1.0), 2) + g(1.0, r * r + s)) / (1.0 + pow(g(1, r * s), 2));
+
+	cout << k;
+
+	return 0;
+}
+
+double g(double x, double y) {
+	return (pow(sin(x), 2) + pow(cos(y), 2)) / (1 + abs(x * y));
+}
